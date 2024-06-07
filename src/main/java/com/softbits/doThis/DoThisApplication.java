d73bbe35c99cba4faa3f29af030e51a3be7ab6ae
Muhammad Qureshi <muhammad83@gmail.com>
2024-06-07 14:29:57 +0100

    spring intial setup

---
+++ src/main/java/com/softbits/doThis/DoThisApplication.java
@@ -0,0 +1,13 @@
+package com.softbits.doThis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoThisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoThisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softbits/doThis/DoThisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softbits.doThis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoThisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
