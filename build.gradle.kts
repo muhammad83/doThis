f6d78ea04b16969a94c85fe10dbc9734919cfc58
Muhammad Qureshi <muhammad83@gmail.com>
2024-06-22 00:29:17 +0100

    added spotless plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 	jacoco
 	id("org.springframework.boot") version "3.3.0"
 	id("io.spring.dependency-management") version "1.1.5"
+	id("com.diffplug.spotless") version "7.0.0.BETA1"
 }
 
 group = "com.softbits"
