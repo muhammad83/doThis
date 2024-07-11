5aa8e93d384c19e40454b9872984dcaf33644614
Muhammad Qureshi <muhammad83@gmail.com>
2024-07-12 00:17:44 +0100

    added google java linter and removed kotlin linter

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-	java
-	jacoco
-	id("org.springframework.boot") version "3.3.0"
-	id("io.spring.dependency-management") version "1.1.5"
-	id("com.diffplug.spotless") version "7.0.0.BETA1"
+    java
+    jacoco
+    id("org.springframework.boot") version "3.3.0"
+    id("io.spring.dependency-management") version "1.1.5"
+    id("com.diffplug.spotless") version "7.0.0.BETA1"
 
 	id("com.dorongold.task-tree") version "4.0.0"
 
@@ -12,46 +12,48 @@ plugins {
 group = "com.softbits"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
 }
 
 jacoco {
-	toolVersion = "0.8.11"
-	reportsDirectory = layout.buildDirectory.dir("jacocoReportDir")
+    toolVersion = "0.8.11"
+    reportsDirectory = layout.buildDirectory.dir("jacocoReportDir")
 }
 
 spotless {
-    kotlin { ktlint() }
+    java {
+        googleJavaFormat()
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
 
 tasks.test {
-	finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
 }
 tasks.jacocoTestReport {
-	dependsOn(tasks.test) // tests are required to run before generating the report
+    dependsOn(tasks.test) // tests are required to run before generating the report
 }
 
 tasks.jacocoTestReport {
-	reports {
-		xml.required = false
-		csv.required = true
-		html.outputLocation = layout.buildDirectory.dir("jacocoHtml")
-	}
+    reports {
+        xml.required = false
+        csv.required = true
+        html.outputLocation = layout.buildDirectory.dir("jacocoHtml")
+    }
 }
